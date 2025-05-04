279d00be3df7893258640a5135a3fef1a10220ea
Veeresh <veereshediga111@gmail.com>
2025-05-05 00:20:59 +0530

    navbar components is finished

---
+++ src/components/footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div>
+      <h1>Footer Components</h1>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,185 @@
+import Button from "react-bootstrap/Button";
+import React, { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import { FaGripLines } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+function NavbarComponent() {
+  const [isMobile, setIsMobile] = useState(false);
+  const [isLaptop, setIsLaptop] = useState(false);
+  const [isMobileNavbar, setIsMobileNavbar] = useState(false);
+
+  const handleClickSideNavbar = () => {
+    setIsMobileNavbar((prev) => !prev);
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+      setIsLaptop(window.innerWidth >= 769);
+      setIsMobileNavbar(true);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <>
+      <Navbar
+        expand="lg"
+        style={{
+          backgroundColor: "#E3F2FD",
+          padding: "16px 24px",
+          boxShadow: "0 2px 8px rgba(0, 0, 0, 0.05)",
+          position: "sticky",
+          top: 0,
+          zIndex: 1000,
+        }}
+      >
+        <Container fluid style={containerStyle}>
+          <Navbar.Brand
+            href="/"
+            style={{
+              fontWeight: "700",
+              color: "#1976D2",
+              fontSize: "1.6rem",
+              flexShrink: 0,
+              marginRight: "2rem",
+              textDecoration: "none",
+            }}
+          >
+          BuyNxt🤞 
+          </Navbar.Brand>
+          {isMobile && (
+            <Navbar.Toggle
+              aria-controls="navbarScroll"
+              style={buttonStyleToggle}
+              className="d-md-none"
+              onClick={handleClickSideNavbar}
+            >
+              {isMobileNavbar ? <FaGripLines /> : <RxCross2 />}
+            </Navbar.Toggle>
+          )}
+          {isLaptop && (
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "1.5rem",
+                  flexWrap: "wrap",
+                }}
+              >
+                <Nav.Link href="/products" style={navLinkStyle}>
+                  Products
+                </Nav.Link>
+                
+                <Nav.Link href="/cart" style={navLinkStyle}>
+                  Cart
+                </Nav.Link>
+                <Nav.Link href="/blogs" style={navLinkStyle}>
+                  Blogs
+                </Nav.Link>
+                <Nav.Link href="/signin" style={navLinkStyle}>
+                  <Button style={buttonStyle}>Sign In </Button>{" "}
+                </Nav.Link>
+                <Nav.Link href="/login" style={navLinkStyle}>
+                  <Button style={buttonStyle}>Log In </Button>{" "}
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          )}
+        </Container>
+      </Navbar>
+      {isMobileNavbar ||
+        (isMobile && (
+          <Container style={containerStylePopUp}>
+            <Nav.Link href="/products" style={navLinkStyle}>
+              Products
+            </Nav.Link>
+            <Nav.Link href="/cart" style={navLinkStyle}>
+              Cart
+            </Nav.Link>
+            <Nav.Link href="/blogs" style={navLinkStyle}>
+              Blogs
+            </Nav.Link>
+            <Nav.Link href="/signin" style={navLinkStyle}>
+              <Button style={buttonStyle}>Sign In </Button>{" "}
+            </Nav.Link>
+            <Nav.Link href="/login" style={navLinkStyle}>
+              <Button style={buttonStyle}>Log In </Button>{" "}
+            </Nav.Link>
+          </Container>
+        ))}
+    </>
+  );
+}
+
+const navLinkStyle = {
+  color: "#1976D2",
+  fontWeight: 700,
+  fontSize: "1rem",
+  textDecoration: "none",
+  transition: "color 0.2s ease-in-out",
+  cursor: "pointer",
+};
+
+const containerStyle = {
+  display: "flex",
+  flexWrap: "wrap",
+  alignItems: "center",
+  justifyContent: "space-between",
+};
+
+const buttonStyle = {
+  backgroundColor: "#007BFF",
+  color: "white",
+  padding: "10px 20px",
+  border: "none",
+  borderRadius: "5px",
+  cursor: "pointer",
+  fontSize: "16px",
+  fontWeight: "bold",
+  transition: "background-color 0.3s ease",
+};
+
+const buttonStyleToggle = {
+  backgroundColor: "#007BFF",
+  color: "white",
+  padding: "10px 20px",
+  border: "none",
+  borderRadius: "5px",
+  cursor: "pointer",
+  fontSize: "16px",
+  fontWeight: "bold",
+  transition: "background-color 0.3s ease",
+};
+
+
+const containerStylePopUp = {
+  backgroundColor: "#fff",
+  display: "flex",
+  gap: "10px",
+  flexDirection: "column",
+  flexWrap: "wrap",
+  alignItems: "center",
+  justifyContent: "center",
+  padding: "10px",
+  margin: "20px",
+  height: "100%",
+  opacity: 1,
+  transform: "translateY(0)",
+  transition: "all 0.4s ease",
+};
+
+// const hiddenStyle = {
+//   opacity: 0,
+//   transform: "translateY(-20px)",
+//   pointerEvents: "none",
+// };
+
+export default NavbarComponent;
